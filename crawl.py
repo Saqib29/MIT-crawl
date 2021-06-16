ca543c47cd43635133cb80d9546d88db67fe9011
Saqib <saqib29111@gmail.com>
2021-06-16 19:20:51 +0600

    execute javascript, extract date, content and links

--- crawl.py
+++ crawl.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from chrome_option import chrome_option
 import time
+import datetime
 import re 
 
 url = 'https://www.ema.europa.eu/en/news-events/whats-new'
@@ -20,32 +21,39 @@ def load_page():
 
 
 def collect_rows():
-    table = driver.find_element_by_xpath("//body//main//table[2]//tbody")
+    # table = driver.find_element_by_xpath("//body//main//table[2]//tbody")
     rows = driver.execute_script("return document.getElementsByTagName('tr').length")
     return rows
 
 
-
-last_update = driver.find_element_by_xpath('//a[13]')
+month = str(datetime.datetime.now())[:7]
+# last_update = driver.find_element_by_xpath('//a[13]')
+last_update = driver.execute_script(f"""return document.querySelector("a[href='/en/news-events/whats-new/{month}']")""")
 driver.execute_script('arguments[0].click();', last_update)
 
-last_update = driver.find_element_by_xpath('//a[13]')
+last_update = driver.execute_script(f"""return document.querySelector("a[href='/en/news-events/whats-new/{month}']")""")
 total_results = re.search(r"\(\d+\)" ,last_update.text)
 total_results = int(total_results.group(0)[1:-1])
+
+
 print(total_results)
-
-
 rows = collect_rows()
 if rows < total_results:
     load_page()
     rows = collect_rows()
-
-
 print(rows)
+
+
 
 for row in range(1,rows-1):
     r = driver.execute_script(f'return document.querySelector("tbody tr:nth-child({row})")')
-    print(f"{row}  -> "+r.text)
+    date = driver.execute_script(f'return document.querySelector("tbody tr:nth-child({row}) td:nth-child(1)")').text
+    content = driver.execute_script(f'return document.querySelector("tbody tr:nth-child({row}) td:nth-child(2)")')
+    link = content.find_element_by_tag_name("a").get_attribute("href")
+    # print(f"{row}  -> "+r.text)
+    print(date)
+    print(content.text)
+    print(link)
 
     # if row > 10:
     #     break
