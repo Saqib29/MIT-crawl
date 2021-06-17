0ff9a866cdc84c6f29f04468462dd8809a89fb46
Saqib <saqib29111@gmail.com>
2021-06-17 17:30:44 +0600

    add loggin, append results, javascript execution

--- crawl.py
+++ crawl.py
@@ -4,24 +4,31 @@ from chrome_option import chrome_option
 import time
 import datetime
 import re 
+import logging
+
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("EMA(1)")
+logger.setLevel(logging.INFO)
+
 
 url = 'https://www.ema.europa.eu/en/news-events/whats-new'
 
 options = chrome_option()
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 driver.get(url)
-# driver.refresh()
+
 
 def load_page():
-    load = driver.find_element_by_xpath("//a[normalize-space()='Load more']")
+    # load = driver.find_element_by_xpath("//a[normalize-space()='Load more']")
+    load = driver.execute_script("""return document.querySelector("body main div div section div div ul li a")""")
     driver.execute_script('arguments[0].click();', load)
+    logger.info("Page load exists")
     time.sleep(20)
 
 
 def collect_rows():
-    # table = driver.find_element_by_xpath("//body//main//table[2]//tbody")
     rows = driver.execute_script("return document.getElementsByTagName('tr').length")
     return rows
 
@@ -42,20 +49,32 @@ if rows < total_results:
     load_page()
     rows = collect_rows()
 print(rows)
+logger.info("Data collected")
 
 
+date_contents = []
 
 for row in range(1,rows-1):
-    r = driver.execute_script(f'return document.querySelector("tbody tr:nth-child({row})")')
+    # r = driver.execute_script(f'return document.querySelector("tbody tr:nth-child({row})")')
     date = driver.execute_script(f'return document.querySelector("tbody tr:nth-child({row}) td:nth-child(1)")').text
     content = driver.execute_script(f'return document.querySelector("tbody tr:nth-child({row}) td:nth-child(2)")')
     link = content.find_element_by_tag_name("a").get_attribute("href")
-    # print(f"{row}  -> "+r.text)
-    print(date)
-    print(content.text)
-    print(link)
+    
+    date_contents.append({
+        "date" : date,
+        "content" : content.text,
+        "datalink" : link
+    })
+    
+    
 
     # if row > 10:
     #     break
 
+logger.info("crawl finished")
 driver.close()
+
+count = 1
+for data in date_contents:
+    print(f"{count} -> ", data)
+    count += 1  
